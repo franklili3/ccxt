30a1e62ebc41930d8fbe11e59d4b605e378e584d
frank <348104201@qq.com>
2019-08-11 19:47:14 +0800

    add print len

--- examples/py/fetch-okex-btc-ohlcv.py
+++ examples/py/fetch-okex-btc-ohlcv.py
@@ -30,7 +30,7 @@ ohlcv = exchange.fetch_ohlcv(symbol, timeframe, from_timestamp)
 first = ohlcv[0][0]
 last = ohlcv[-1][0]
 print('First candle datetime', exchange.iso8601(first))
-print('Last candle datetime', exchange.iso8601(last))
+print(len(ohlcv), 'Last candle datetime', exchange.iso8601(last))
 time.sleep(exchange.rateLimit / 1000)
 data_df = pd.DataFrame(ohlcv)
 data_df.to_csv('okex_' + symbol + '_' + timeframe + '_' + from_datetime[0:10] + '.csv')
