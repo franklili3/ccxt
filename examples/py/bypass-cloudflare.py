6002e002f5437d28434d0b2fc3977545ed29dc86
frank <348104201@qq.com>
2019-08-12 21:10:44 +0800

    add fetch ohlcv codes

--- examples/py/bypass-cloudflare.py
+++ examples/py/bypass-cloudflare.py
@@ -3,6 +3,7 @@
 import cfscrape
 import os
 import sys
+import csv
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
@@ -14,6 +15,71 @@ def print_supported_exchanges():
     print('Supported exchanges:')
     print(', '.join(ccxt.exchanges))
 
+def retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    num_retries = 0
+    success = False
+    while not success:
+        try:
+            num_retries += 1
+            ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, limit)
+            print('Fetched', len(ohlcv), symbol, 'candles from', exchange.iso8601 (ohlcv[0][0]), 'to', exchange.iso8601 (ohlcv[-1][0]))
+            success = True
+            return ohlcv
+        except Exception:
+            if num_retries > max_retries:
+                raise Exception('Failed to fetch', timeframe, symbol, 'OHLCV in', max_retries, 'attempts')
+
+
+def scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    earliest_timestamp = exchange.milliseconds()
+    timeframe_duration_in_seconds = exchange.parse_timeframe(timeframe)
+    timeframe_duration_in_ms = timeframe_duration_in_seconds * 1000
+    timedelta = limit * timeframe_duration_in_ms
+    all_ohlcv = []
+    while True:
+        fetch_since = earliest_timestamp - timedelta
+        ohlcv = retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, fetch_since, limit)
+        # if we have reached the beginning of history
+        if ohlcv[0][0] >= earliest_timestamp:
+            break
+        earliest_timestamp = ohlcv[0][0]
+        first = ohlcv[0][0]
+        last = ohlcv[-1][0]
+        print('First candle datetime', exchange.iso8601(first))
+        print('Last candle datetime', exchange.iso8601(last))
+        all_ohlcv = ohlcv + all_ohlcv
+        print(len(all_ohlcv), 'candles in total from', exchange.iso8601(all_ohlcv[0][0]), 'to', exchange.iso8601(all_ohlcv[-1][0]))
+        # if we have reached the checkpoint
+        if fetch_since < since:
+            break
+    return all_ohlcv
+
+
+def write_to_csv(filename, data):
+    with open(filename, mode='w') as output_file:
+        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerows(data)
+
+
+def scrape_candles_to_csv(filename, id, max_retries, symbol, timeframe, since, limit, exchange):
+    # instantiate the exchange by id
+    #exchange = getattr(ccxt, exchange_id)({
+    #    'enableRateLimit': True,  # required by the Manual
+    #})
+    #exchange = getattr(ccxt, id)({
+            #'timeout': 20000,
+            #'session': cfscrape.create_scraper(),
+    #})
+    # convert since from string to milliseconds integer if needed
+    if isinstance(since, str):
+        since = exchange.parse8601(since)
+    # preload all markets from the exchange
+    #exchange.load_markets()
+    # fetch all candles
+    ohlcv = scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit)
+    # save them to csv file
+    write_to_csv(filename, ohlcv)
+    print('Saved', len(ohlcv), 'candles from', exchange.iso8601(ohlcv[0][0]), 'to', exchange.iso8601(ohlcv[-1][0]), 'to', filename)
 
 try:
 
@@ -39,8 +105,8 @@ try:
 
             # output a list of all market symbols
             print(id + ' has ' + str(len(exchange.symbols)) + ' symbols: ' + ', '.join(exchange.symbols))
-            print(id + ' has ' + str(len(exchange.symbols)) + ' symbols: ' + ', '.join(exchange.symbols))
             print('Succeeded.')
+            scrape_candles_to_csv(id + '_2016-01-01.csv', id, 10, 'BTC/USDT', '1m', '2016-01-01T00:00:00Z', 1000, exchange)
 
         except ccxt.BaseError as e:
 
